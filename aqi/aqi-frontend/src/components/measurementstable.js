9e63271e10844041a8be9b5e43e1f9bcf218a47b
Belle Williams <b.williams0220232@arts.ac.uk>
2024-06-08 12:31:47 +0100

    updated front end aqi react website design

--- aqi/aqi-frontend/src/components/measurementstable.js
+++ aqi/aqi-frontend/src/components/measurementstable.js
@@ -1,8 +1,11 @@
+// src/components/MeasurementsTable.js
 import React, { useState, useEffect } from 'react';
 
+// MeasurementsTable component
 const MeasurementsTable = () => {
   const [measurements, setMeasurements] = useState([]);
 
+  // Fetch data from the API when the component mounts
   useEffect(() => {
     fetch('http://127.0.0.1:5100/json/location/1/measurements')
       .then(response => {
@@ -21,12 +24,16 @@ const MeasurementsTable = () => {
   return (
     <div>
       <h1>Measurements</h1>
+      {/* Render the table with measurements data */}
       <table>
         <thead>
           <tr>
             <th>ID</th>
             <th>Parameter</th>
             <th>Value</th>
+            <th>Date</th>
+            <th>Location ID</th>
+            <th>Parameter ID</th>
           </tr>
         </thead>
         <tbody>
@@ -36,11 +43,14 @@ const MeasurementsTable = () => {
                 <td>{measurement.id}</td>
                 <td>{measurement.parameter}</td>
                 <td>{measurement.value}</td>
+                <td>{measurement.date}</td>
+                <td>{measurement.location_id}</td>
+                <td>{measurement.parameter_id}</td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="3">No data available</td>
+              <td colSpan="6">No data available</td>
             </tr>
           )}
         </tbody>
