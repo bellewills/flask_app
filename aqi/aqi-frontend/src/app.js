933c6ffbf9a04a9d5db23d240b30873a574561f3
Belle Williams <b.williams0220232@arts.ac.uk>
2024-06-09 15:12:55 +0100

    adding comments

--- aqi/aqi-frontend/src/app.js
+++ aqi/aqi-frontend/src/app.js
@@ -10,16 +10,26 @@ function App() {
   return (
     <Router>
       <div className="app">
+        {/* Navigation bar */}
         <nav>
           <ul>
+            {/* Link to the Home page */}
             <li><Link to="/" className="home-link">Home</Link></li>
           </ul>
         </nav>
+        
+        {/* Main title of the application */}
         <h1 className="title">Air Quality Database</h1>
+        
+        {/* Define routes for different components */}
         <Routes>
+          {/* Route for the Home component */}
           <Route path="/" element={<Home />} />
+          {/* Route for the LocationsTable component */}
           <Route path="/locations" element={<LocationsTable />} />
+          {/* Route for the ParametersTable component */}
           <Route path="/parameters" element={<ParametersTable />} />
+          {/* Route for the MeasurementsTable component */}
           <Route path="/measurements" element={<MeasurementsTable />} />
         </Routes>
       </div>
