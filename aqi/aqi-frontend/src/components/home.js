9bb0d709f8fcfebc9a7eac37f5cdf5c4b3d68927
Belle Williams <b.williams0220232@arts.ac.uk>
2024-06-09 15:07:17 +0100

    adding comments

--- aqi/aqi-frontend/src/components/home.js
+++ aqi/aqi-frontend/src/components/home.js
@@ -5,15 +5,23 @@ import '../index.css';
 // Home component
 function Home() {
   return (
+    // Main container for the Home component
     <div className="home-container">
+      
+      {/* Background video container */}
       <div className="bg-video">
+        
+        {/* Video element with autoplay, muted, and loop properties */}
         <video autoPlay muted loop className="bg-video__content">
           <source src={`${process.env.PUBLIC_URL}/3129724-uhd_3840_2160_30fps.mp4`} type="video/mp4" />
           Your browser is not supported! :/
         </video>
       </div>
+      
+      {/* Central container for the navigation links */}
       <div className="center-container">
         
+        {/* List of navigation links */}
         <ul className="center-links">
           <li><Link to="/locations" className="button-link">Locations</Link></li>
           <li><Link to="/parameters" className="button-link">Parameters</Link></li>
