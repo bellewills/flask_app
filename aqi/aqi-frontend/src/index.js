5903161f61e126ee7775b43a10d7957894d5327d
Belle Williams <b.williams0220232@arts.ac.uk>
2024-06-09 15:18:35 +0100

    added comments

--- aqi/aqi-frontend/src/index.js
+++ aqi/aqi-frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+import App from './App';
 
+// Render the App component into the root div
 ReactDOM.render(
+  // Use React.StrictMode to highlight potential problems in the application
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // Mount the App component to the div with the id 'root'
   document.getElementById('root')
 );
